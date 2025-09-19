c8c6f28646e2644ed90bfc6413679866d33ad07f
sansan296 <sanshizu429@gmail.com>
2025-09-19 18:34:13 +0900

    検索機能の追加

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-// 🔽 追加
-use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\TweetLikeController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
   return view('welcome');
@@ -19,10 +18,11 @@ Route::middleware('auth')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  // 🔽 検索のルーティングを追加
+  Route::get('/tweets/search', [TweetController::class, 'search'])->name('tweets.search');
   Route::resource('tweets', TweetController::class);
   Route::post('/tweets/{tweet}/like', [TweetLikeController::class, 'store'])->name('tweets.like');
   Route::delete('/tweets/{tweet}/like', [TweetLikeController::class, 'destroy'])->name('tweets.dislike');
-  // 🔽 追加
   Route::resource('tweets.comments', CommentController::class);
 });
 
