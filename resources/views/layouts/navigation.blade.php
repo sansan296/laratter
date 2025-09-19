232a1e6efbd0b9b8a8b50a2a4b559fb14f9e67cc
sansan296 <sanshizu429@gmail.com>
2025-09-19 20:12:23 +0900

    ユーザページの実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,9 +21,11 @@
           <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
             {{ __('Tweet作成') }}
           </x-nav-link>
-          <!-- 🔽 リンク追加 -->
           <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
             {{ __('Tweet検索') }}
+          </x-nav-link>
+          <x-nav-link :href="route('profile.show', auth()->user())" :active="request()->routeIs('profile.show')">
+            {{ __('マイページ') }}
           </x-nav-link>
         </div>
       </div>
@@ -86,9 +88,11 @@
       <x-responsive-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
         {{ __('Tweet作成') }}
       </x-responsive-nav-link>
-      <!-- 🔽 リンク追加 -->
       <x-responsive-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
         {{ __('Tweet検索') }}
+      </x-responsive-nav-link>
+      <x-responsive-nav-link :href="route('profile.show', auth()->user())" :active="request()->routeIs('profile.show')">
+        {{ __('マイページ') }}
       </x-responsive-nav-link>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware('auth')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-  // 🔽 検索のルーティングを追加
+  Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
   Route::get('/tweets/search', [TweetController::class, 'search'])->name('tweets.search');
   Route::resource('tweets', TweetController::class);
   Route::post('/tweets/{tweet}/like', [TweetLikeController::class, 'store'])->name('tweets.like');
